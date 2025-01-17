be99307b430dc8ab5d0542e790ee387bc1a94b44
cooleHoonst <88084780+89JHoon@users.noreply.github.com>
2025-01-17 19:53:10 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/example/whereisthisvod/WhereisthisVodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whereisthisvod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereisthisVodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
